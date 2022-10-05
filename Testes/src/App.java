dedc5fcf6bd94f6402f7cc34eca368093cb1bf42
DeexDeveloper <63799905+DeexDeveloper@users.noreply.github.com>
2022-10-05 14:54:14 -0300

    uns testes

--- Testes/src/App.java
+++ Testes/src/App.java
@@ -1,9 +1,14 @@
-import javax.swing.SortingFocusTraversalPolicy;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         Testando testando = new Testando("ronaldo", "5456456456", 3462251, 6000f);
         testando.promover(20f);
-        System.out.print(testando.getSalario());
+        System.out.println(testando.getSalario());
+        System.out.println(testando);
+        testando.setFuncionario(testando.getFuncionario().toUpperCase());
+        System.out.println(testando.getFuncionario());
+        
     }
+
 }
--- Testes/src/Testando.java
+++ Testes/src/Testando.java
@@ -1,9 +1,41 @@
 public class Testando {
-    String funcionario;
-    String cpf;
-    long telefone;
-    Float salario;
+    private String funcionario;
+    private String cpf;
+    private long telefone;
+    private Float salario;
 
+
+    public String getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(String funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+    public Float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Float salario) {
+        this.salario = salario;
+    }
 
     public Testando(String funcionario, String cpf, long telefone, Float salario) {
         this.funcionario = funcionario;
@@ -14,26 +46,6 @@ public class Testando {
     
     public void promover (Float percent){
         this.salario*=1+(percent/100);
-    }
-
-
-    public String getFuncionario() {
-        return funcionario;
-    }
-
-
-    public String getCpf() {
-        return cpf;
-    }
-
-
-    public long getTelefone() {
-        return telefone;
-    }
-
-
-    public Float getSalario() {
-        return salario;
     }
 
 }
